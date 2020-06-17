d6e62ee487e094f543bc7cbd3a27d799ec54d007
Saltyric <916545108@qq.com>
2020-06-17 11:29:06 +0800

    main project update

--- PracticeProject1/plane_main.py
+++ PracticeProject1/plane_main.py
@@ -2,7 +2,8 @@ import pygame
 from plane_sprites import *
 
 #   屏幕大小的常量
-SCREEN_RECT = pygame(0, 0, 480, 700)
+SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
+
 
 class PlanGame(object):
     """飞机大战主游戏"""
@@ -11,7 +12,7 @@ class PlanGame(object):
         print("游戏初始化")
 
         #   创建游戏窗口
-        self.screen = pygame.display.set_mode(SCREEN_RECT)
+        self.screen = pygame.display.set_mode(SCREEN_RECT.size)
         #   创建游戏时钟
         self.clock = pygame.time.Clock()
         #   调用私有方法，精灵和精灵组的创建
@@ -22,6 +23,17 @@ class PlanGame(object):
 
     def start_game(self):
         print("游戏开始...")
+        while True:
+            for event in pygame.event.get():
+                #   判断用户是否点击了关闭按钮
+                if event.type == pygame.QUIT:
+                    print("退出游戏")
+
+                    #   quit卸载所以模块
+                    pygame.quit()
+
+                    #   退出系统
+                    exit()
 
 
 if __name__ == '__main__':
